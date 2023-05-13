24b44d3dbd1817409da8b1a43d72fd4e338973d7
ivanBlade <itsesparta@gmail.com>
2023-05-13 18:12:53 -0500

    A gregando Swagger

--- catalogo/src/main/java/com/example/catalogo/CatalogoApplication.java
+++ catalogo/src/main/java/com/example/catalogo/CatalogoApplication.java
@@ -1,7 +1,11 @@
 package com.example.catalogo;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CatalogoApplication {
@@ -10,4 +14,14 @@ public class CatalogoApplication {
         SpringApplication.run(CatalogoApplication.class, args);
     }
 
+    @Bean
+    public OpenAPI custoOpenAPI() {
+        return new OpenAPI().info(new Info()
+                .title("OPEN API MICROSERVICIOS CATALOGO")
+                .version("0.0.1")
+                .description("servicio web catalogo")
+                .termsOfService("http://swagger.io/terms")
+                .license(new License().name("Apache 2.0").url("http://springdoc.org"))
+        );
+    }
 }
