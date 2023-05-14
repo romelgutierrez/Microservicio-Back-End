a903adca6937b6c028284c3895b7d6f650d5d481
ivanBlade <itsesparta@gmail.com>
2023-05-13 20:19:28 -0500

    Gateway correccion*

--- gateway-server/src/main/java/com/example/gatewayserver/GatewayServerApplication.java
+++ gateway-server/src/main/java/com/example/gatewayserver/GatewayServerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GatewayServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServerApplication.class, args);
+    }
 
 }
